d3d17bd1260c8c1cec5ce084028530b1e7de4e8c
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2023-10-19 19:27:45 +0200

    Ostateczne rozwiązanie zadania

--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
 import math
 
-
-def czynnikiPierwsze(n):
-    czynniki = []
+def primeFactors(n):
+    factors = []
 
     if n <= 1:
         return
 
     while n % 2 == 0:
-        czynniki.append(2)
+        factors.append(2)
         n = n // 2
 
     for i in range(3, int(math.sqrt(n)) + 1, 2):
         while n % i == 0:
-            czynniki.append(i)
+            factors.append(i)
             n = n // i
 
     if n > 2:
-        czynniki.append(n)
+        factors.append(n)
 
-    return czynniki
+    return factors
 
 
-n = int(input("Podaj liczbę całkowitą: "))
-wynik = czynnikiPierwsze(n)
-print(wynik)
+if __name__ == "__main__":
+    n = int(input("Enter an integer: "))
+    result = primeFactors(n)
+    print(result)
--- test/test.py
+++ test/test.py
@@ -1,14 +1,14 @@
-import os
-
 import pytest
 
-from src.main import czynnikiPierwsze
+from src.main import primeFactors
 
+def test_prime_factors():
+    assert primeFactors(20) == [2, 2, 5]
+    assert primeFactors(1) is None
+    assert primeFactors(2) == [2]
+    assert primeFactors(17) == [17]
+    assert primeFactors(60) == [2, 2, 3, 5]
+    assert primeFactors(144) == [2, 2, 2, 2, 3, 3]
 
-def testCzynnikiPierwsze():
-    assert czynnikiPierwsze(20) == [2, 2, 5]
-    assert czynnikiPierwsze(1) == "return"
-    assert czynnikiPierwsze(2) == [2]
-    assert czynnikiPierwsze(17) == [17]
-    assert czynnikiPierwsze(60) == [2, 2, 3, 5]
-    assert czynnikiPierwsze(144) == [2, 2, 2, 2, 3, 3]
+if __name__ == "__main__":
+    pytest.main()
